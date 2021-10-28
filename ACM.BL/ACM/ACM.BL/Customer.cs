9d6ae8ad7b58ef44c066981f9d57d512a2b571ba
Guilherme de Oliveira Teixeira <guilherme.teixeira2303@gmail.com>
2021-10-28 13:14:44 -0300

    Created variables, and Inputs

---
+++ ACM.BL/ACM/ACM.BL/Customer.cs
@@ -0,0 +1,27 @@
+﻿namespace ACM.BL
+{
+    public class Customer
+    {
+        public Customer(int customerId, string emailAdress, string lastName, string firstName)
+        {
+            CustomerId = customerId;
+            EmailAdress = emailAdress;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public int CustomerId { get; private set; }
+        public string EmailAdress { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+       }
+
+    }
+}
