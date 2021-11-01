1713914bdcf4297a2f679de0ff380a71f1686ee3
Guilherme de Oliveira Teixeira <guilherme.teixeira2303@gmail.com>
2021-11-01 17:20:04 -0300

    Started propertys and how to manipulate them

---
+++ CCodeAcademy/Classes/Forest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BasicClasses
+{
+    class Forest
+    {
+        //public string name; 
+        public int trees;
+        public int age;
+        public string biome;
+
+        public string name { get; set; } // Resumed version of the string
+    }
+}
---
+++ CCodeAcademy/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest();
+            f.name = "Amazonia";
+            f.trees = 96525;
+            f.age = 7000;
+            f.biome = "Equatorial";
+
+            Console.WriteLine(f.name);
+        }
+    }
+}
