2dc246b2d51a7725b5a1bf4e996902136309e7aa
Guilherme de Oliveira Teixeira <guilherme.teixeira2303@gmail.com>
2021-11-03 10:35:06 -0300

    Finished how to manipulate classes, properties, using void

--- CCodeAcademy/Classes/Forest.cs
+++ CCodeAcademy/Classes/Forest.cs
@@ -9,11 +9,97 @@ namespace BasicClasses
 {
     class Forest
     {
-        //public string name; 
-        public int trees;
-        public int age;
-        public string biome;
+        // FIELDS
 
-        public string name { get; set; } // Resumed version of the string
+        public int age;
+        private string biome;
+        private static int forestsCreated;
+        private static string treeFacts;
+
+        // CONSTRUCTORS
+
+        public Forest(string name, string biome)
+        {
+            this.Name = name;
+            this.Biome = biome;
+            Age = 0;
+            ForestsCreated++;
+        }
+
+        public Forest(string name) : this(name, "Unknown")
+        { }
+        static Forest()
+        {
+            treeFacts = "Forests provide a diversity of ecosystem services including:\r\n  aiding in regulating climate.\r\n  purifying water.\r\n  mitigating natural hazards such as floods.\n";
+            ForestsCreated = 0;
+        }
+
+
+
+        // PROPERTIES
+
+        public string Name
+        { get; set; }
+
+        public int Trees
+        { get; set; }
+
+        public string Biome
+        {
+            get { return biome; }
+            set
+            {
+                if (value == "Tropical" ||
+                    value == "Temperate" ||
+                    value == "Boreal")
+                {
+                    biome = value;
+                }
+                else
+                {
+                    biome = "Unknown";
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        public static int ForestsCreated
+        {
+            get { return forestsCreated; }
+            private set { forestsCreated = value; }
+        }
+
+        public static string TreeFacts
+        {
+            get { return treeFacts; }
+        }
+
+        // METHODS
+
+        public int Grow()
+        {
+            Trees += 30;
+            Age += 1;
+            return Trees;
+        }
+
+        public int Burn()
+        {
+            Trees -= 20;
+            Age += 1;
+            return Trees;
+        }
+
+        public static void PrintTreeFacts()
+        {
+            Console.WriteLine(TreeFacts);
+        }
+
+
     }
 }
--- CCodeAcademy/Classes/Program.cs
+++ CCodeAcademy/Classes/Program.cs
@@ -7,13 +7,23 @@ namespace BasicClasses
     {
         static void Main(string[] args)
         {
-            Forest f = new Forest();
-            f.name = "Amazonia";
-            f.trees = 96525;
-            f.age = 7000;
-            f.biome = "Equatorial";
+            //Forest f = new Forest();
+            //f.name = "Amazonia";
+            //f.trees = 96525;
+            //f.age = 7000;
+            //f.biome = "Equatorial";
 
-            Console.WriteLine(f.name);
+            //Console.WriteLine(f.name);
+            Forest f = new Forest("Rendlesham");
+
+            Console.WriteLine(f.Name);
+            Console.WriteLine(f.Biome);
+            Forest.PrintTreeFacts();
+
+            f.Grow();
+
+            Console.WriteLine(Forest.TreeFacts);
+
         }
     }
 }
